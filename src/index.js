044f5ff53b93abef63703c50f0daa08854977700
Dani Lyn <dlyn1110@gmail.com>
2020-04-24 00:01:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //initialize taskList class
   const taskList = new TaskList();
+  //grab all necessary DOM elements
+
+  //form and relevant input fileds
+  const newTaskForm = document.getElementById('create-task-form');
+  const newTaskDescription = document.getElementById('create-task-descrption')
+
 });
