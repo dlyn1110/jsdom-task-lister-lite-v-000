18e4a233749d431350e2dea198c9e89f48031ef0
Dani Lyn <dlyn1110@gmail.com>
2020-04-24 00:19:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // initialize taskList class
-  const taskList = new TaskList();
-  //grab all the necessary DOM elements
+  const form = document.getElementById('create-task-form');
+   const taskDescription = document.getElementById('new-task-description');
+   
+   function addTasks (event) {
+     const task = document.createElement('li'); // Create a task.
 
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  const taskUl = document.getElementById("tasks");
-
-  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
-  //attach event listeners
-
-  newTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    renderApp();
-  });
-
-  taskUl.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      taskList.deleteTask(e.target.dataset.description);
-      renderApp();
-    }
-  });
-});
+     task.textContent = taskDescription.value + " ";
