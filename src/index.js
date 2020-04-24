b65b79e92d07590a84e63539c8002aa7509968e1
Dani Lyn <dlyn1110@gmail.com>
2020-04-24 00:33:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
    task.textContent = taskDescription.value + " ";
 
    // Create the delete button for the task.
-   const deleteButton = document.createElement('button');
-   deleteButton.setAttribute("data-description", taskDescription.value);
-   deleteButton.textContent = "Delete";
+   //const deleteButton = document.createElement('button');
+   //deleteButton.setAttribute("data-description", taskDescription.value);
+   //deleteButton.textContent = "Delete";
 
    // Add an event listener for deleting the task. THIS is where that functionality belongs!
    // For future reference, I think it's a good idea to add event listeners to tasks as they are being created.
-   deleteButton.addEventListener('click', function () { task.remove(); });
+   //deleteButton.addEventListener('click', function () { task.remove(); });
 
-   task.appendChild(deleteButton); // Add the delete button to the task.
+   //task.appendChild(deleteButton); // Add the delete button to the task.
    tasks.appendChild(task); // Add the task to the TODO List.
 
    taskDescription.value = ""; // Reset the taskDescription input.
